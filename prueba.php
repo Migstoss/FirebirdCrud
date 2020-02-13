a586de155585574d7f990bc201b50a99e602e1bb
Migstoss <43889179+Migstoss@users.noreply.github.com>
2020-02-13 08:35:24 -0500

    Remover prueba.php

--- prueba.php
+++
@@ -1,6 +0,0 @@
-<label for="update">Click me</label>
-<a href="update.php"><input  id="update" name="update" title="Editar" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-
-<br>
-<label for="Name">Click me</label>
-<input type="text" id="Name" name="Name" />
